40c15e6c16d67561fa74976fb1f4f12bdb0ece34
Tevin Dikgale <tevindikgale@gmail.com>
2023-05-06 21:46:27 +0200

    First commit

---
+++ README.md
@@ -0,0 +1,11 @@
+# mypackage
+This library was created as an example of how to publish your own Python Package.
+
+# build this package locally
+`python setup.py sdist`
+
+## installing this package from Github
+`pip install git+https://github.com/tevindikgale/mypackage.git`
+
+## updating this package from Github
+`pip install --upgrade git+https://github.com/tevindikgale/mypackage.git`
---
+++ mypackages/myModule.py
@@ -0,0 +1,32 @@
+def fibonacci(n):
+
+    """
+    Calculate nth term in fibonacci sequence
+    
+    Args:
+        n (int): nth term in fibonacci sequence to calculate
+    
+    Returns:
+        int: nth term of fibonacci sequence,
+             equal to sum of previous two terms
+    
+    Examples:
+        >>> fibonacci(1)
+        1        
+        >> fibonacci(2)
+        1
+        >> fibonacci(3)
+        2
+    """
+
+    for i in range(n):  # Keep sorting until we have the top n items
+        for j in range(len(items)-1-i):
+
+            if items[j] > items[j+1]:  # If this item is bigger than next the item..
+                items[j], items[j+1] = items[j+1], items[j]  # swap the two!
+                
+    # Get last two items
+    top_n = items[-n:]
+    
+    # Return in descending order
+    return top_n[::-1]
